db26253a89bd69ccdd0700acbcc812a5ccbde7cf
richelbilderbeek <richel@richelbilderbeek.nl>
2017-10-29 14:30:20 +0100

    Use libavutil-dev instead of ffmpeg

--- SetupThrive.rb
+++ SetupThrive.rb
@@ -64,7 +64,7 @@ if BuildPlatform == "linux" and not SkipPackageManager
 	PackagesToInstall = "libbullet-dev libboost-dev build-essential automake libtool " +
                         "libfreetype6-dev libfreeimage-dev libzzip-dev libxrandr-dev " +
                         "libxaw7-dev freeglut3-dev libgl1-mesa-dev libglu1-mesa-dev " +
-                        "libois-dev libboost-thread-dev libtinyxml-dev libglm-dev ffmpeg " +
+                        "libois-dev libboost-thread-dev libtinyxml-dev libglm-dev libavutil-dev " +
                         "libavutil-dev libopenal-dev"
 
   elsif LinuxOS.casecmp("Arch") == 0
